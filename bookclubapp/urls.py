d671954feeabf4fc288a05f1578c0cbbad13d037
Kiptim <kiptim54@gmail.com>
2018-05-30 13:16:38 +0300

    add api search function working

--- bookclubapp/urls.py
+++ bookclubapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page' )
+    path('', views.landing_page, name='landing_page' ),
+    path('search/', views.search_book, name='search_book'),
 ]
--- bookclubapp/views.py
+++ bookclubapp/views.py
@@ -6,7 +6,16 @@ from .request import search_books
 # Create your views here.
 
 def landing_page(request):
-    book_searched=search_books('purple')
+    book_searched=search_books('chicklit')
     
 
     return render(request, 'index.html', {"books":book_searched})
+
+def search_book(request):
+    title="Book Results"
+    if 'searchbook' in request.GET and request.GET['searchbook']:
+        search_term=request.GET.get('searchbook')
+        book_searched=search_books(search_term)
+        
+
+    return render(request, 'searched_book.html', {"books":book_searched})
